aeb5db71e5768e8239fb7fdbb8fc9c369c12cd75
TXZR <1464328049@qq.com>
2023-04-02 15:37:16 +0800

    commit

--- src/main/java/com/example/pro/controller/ProController.java
+++ src/main/java/com/example/pro/controller/ProController.java
@@ -1,15 +1,17 @@
 package com.example.pro.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.example.pro.config.TywRestrictAspectHadler;
 import com.example.pro.dto.LCQueryDto;
 import com.example.pro.dto.TywResponse;
 import com.example.pro.service.ProService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class ProController {
@@ -26,6 +28,7 @@ public class ProController {
 
     @RequestMapping(value = "/query", method = RequestMethod.POST)
     public Object query(@RequestBody LCQueryDto lcQueryDto) {
+        log.info("query param:{}", JSON.toJSONString(lcQueryDto));
         return TywResponse.success(proService.query(lcQueryDto));
     }
 }
